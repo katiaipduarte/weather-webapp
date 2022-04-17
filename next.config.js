0629f532ca391fd110cbbb0af464312f62e55e2f
Katia Duarte <katiaipduarte@hotmail.com>
2022-04-17 12:54:56 +0100

    feat: add service to get the weather. add example with default coord. add env example to run the example locally with the needed key. add open weather api interfaces

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+	//needs to be false because the new strict mode since React 18 default behaviors rusn twice.
+	// See more: https://reactjs.org/blog/2022/03/29/react-v18.html#new-strict-mode-behaviors
+	reactStrictMode: false,
+	compiler: {
+		styledComponents: true,
+	},
 }
 
 module.exports = nextConfig
